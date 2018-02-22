cc536ebd4610da1f7645695727020728723b2f57
Jonathan Bogdanovicz <jhonnybail@gmail.com>
2018-02-22 13:37:53 +0000

    Melhorias na forma de preencher valores da entidade por array

--- src/AbstractEntity.php
+++ src/AbstractEntity.php
@@ -10,11 +10,12 @@
 namespace Solutio;
 
 use Zend\Hydrator,
+    Doctrine\ORM\Mapping as ORM,
+    Doctrine\Common\Annotations\AnnotationReader,
     Solutio\Utils\Data\StringManipulator;
 
 abstract class AbstractEntity implements \JsonSerializable
 {
-
   protected static $primaryKeys = ['id'];
 
   public function __construct($options = [])
@@ -28,18 +29,47 @@ abstract class AbstractEntity implements \JsonSerializable
   public function fromArray(array $data)
   {
     $reflection = \Zend\Server\Reflection::reflectClass($this);
-    foreach($reflection->getMethods() as $v){
-      $name = lcfirst((new StringManipulator($v->getName()))->replace('^set', ''));
-      if(!empty($data[$name])){
-        if(count($v->getPrototypes()) > 1){
-          $type = $v->getPrototypes()[1]->getParameters()[0]->getType();
-          if(class_exists($type)){
-            if(isset($data[$name]) && !($data[$name] instanceof \Solutio\AbstractEntity)){
-              $data[$name] = new $type($data[$name] instanceof \Solutio\Utils\Data\ArrayObject ? (array) $data[$name] : $data[$name]);
+    $pending    = [];
+    foreach($reflection->getProperties() as $property){
+      $method = 'get' . ucfirst($property->getName());
+      $name = $property->getName();
+      $propertyAnnotations = $this->getAnnotationReader()->getPropertyAnnotations($property);
+      if(array_key_exists($name, $data) && is_null($data[$name])){
+        $method = str_replace('get', 'set', $method);
+        $this->{$method}(null); 
+      }
+      foreach($propertyAnnotations as $propertyAnnotation)
+        if($propertyAnnotation instanceof ORM\ManyToOne || $propertyAnnotation instanceof ORM\OneToOne){
+          if(array_key_exists($name, $data)){
+            $className  = preg_match('/\\\/', $propertyAnnotation->targetEntity) ? $propertyAnnotation->targetEntity : $reflection->getNamespaceName() . '\\' . $propertyAnnotation->targetEntity;
+            $className = StringManipulator::GetInstance($className)->replace('DoctrineORMModule\\\Proxy\\\__CG__\\\\', '')->toString();
+            if(array_key_exists($name, $data) && !($data[$name] instanceof $className)){
+              $newEntity = new $className(($data[$name] instanceof \Traversable || is_array($data[$name])) ? (array) $data[$name] : $data[$name]);
+              if(!(is_array($propertyAnnotation->cascade) && in_array('persist', $propertyAnnotation->cascade))){
+                $data[$name]  = $newEntity->getKeys();
+                foreach($data[$name] as $v){
+                  if(is_null($v) || $v === ""){
+                    $data[$name] = null;
+                    break;
+                  }
+                }
+                if(is_null($data[$name]))
+                  continue;
+                $newEntity    = new $className($data[$name]);
+              }
+              if($this->{$method}() instanceof $className
+                  && $newEntity->getKeys() === $this->{$method}()->getKeys()
+                  && ! $data[$name] instanceof \Doctrine\ORM\Proxy\Proxy){
+                foreach($data[$name] as $k => $v)
+                  if($v === $this)
+                    unset($data[$name][$k]);
+                $this->{$method}()->fromArray($data[$name]);
+                unset($data[$name]);
+              }else
+                $data[$name] = $newEntity;
             }
           }
         }
-      }
     }
     (new Hydrator\ClassMethods)->hydrate($data,$this);
   }
@@ -56,15 +86,37 @@ abstract class AbstractEntity implements \JsonSerializable
         unset($obj[$k]);
     return $obj;
   }
+  
+  public function getKeys() : array
+  {
+    $keys   = self::NameOfPrimaryKeys();
+    $return = [];
+    foreach($keys as $key){
+      $methodName = 'get' . ucfirst($key);
+      $return[$key] = $this->{$methodName}();
+      if($this->{$methodName}() instanceof AbstractEntity){
+        $return[$key] = $this->{$methodName}()->getKeys();
+        if(count($return[$key]) === 1)
+          $return[$key] = current($return[$key]);
+      }
+    }
+    return $return;
+  }
 
   public static function NameOfPrimaryKeys()
   {
     return static::$primaryKeys;
   }
 
+  protected function getAnnotationReader() : AnnotationReader
+  {
+    if(empty($this->annotationReader))
+      $this->annotationReader = new AnnotationReader;
+    return $this->annotationReader;
+  }
+
   public function jsonSerialize()
   {
     return $this->toArray();
   }
-
 }
